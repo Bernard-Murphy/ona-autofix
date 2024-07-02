8d1345b416d586347fccf7a442acd41701ee717c
Bernard <bernard.murphy@rutgers.edu>
2024-07-02 06:04:31 -0400

    more tests

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const interval = process.env.INTERVAL;
 
 const run = async () => {
   try {
-    if (!fs.existsSync("/var/opt/remi/php82/run/php-fpm/www.sock")) {
-      console.log("Forum down", new Date());
-      exec("sudo sh -c 'sh restore.sh'", (err, stdout, stderr) => {
-        if (err) console.log("err", err);
-        if (stderr) console.log("stderr", stderr);
-        if (stdout) console.log("stdout", stdout);
-      });
-    }
+    // if (!fs.existsSync("/var/opt/remi/php82/run/php-fpm/www.sock")) {
+    console.log("Forum down", new Date());
+    exec("sudo sh -c 'sh restore.sh'", (err, stdout, stderr) => {
+      if (err) console.log("err", err);
+      if (stderr) console.log("stderr", stderr);
+      if (stdout) console.log("stdout", stdout);
+    });
+    // }
     // purgeChats();
   } catch (err) {
     console.log("An error occurred:\n", err);
