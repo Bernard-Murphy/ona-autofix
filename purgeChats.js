a97f6e67e22a1148eaaaa11c33f05fc1f6183de4
Bernard <bernard.murphy@rutgers.edu>
2024-07-02 06:03:38 -0400

    safe

--- purgeChats.js
+++ purgeChats.js
@@ -12,22 +12,22 @@ const purgeChats = () =>
 
       sql.connect();
 
-      sql.query(
-        `update xf_conversation_message set user_id = "0", username = "guest", message = "This message has been pruned" where message_date < ${
-          new Date(new Date().setMonth(new Date().getMonth() - 1)).getTime() *
-          1000
-        } and message != "This message has been pruned"`,
-        (err, results) => {
-          try {
-            if (err) throw err;
-            else {
-              console.log(`${results.changedRows} messages pruned`);
-            }
-          } catch (err) {
-            console.log("Error", err);
-          }
-        }
-      );
+      //   sql.query(
+      //     `update xf_conversation_message set user_id = "0", username = "guest", message = "This message has been pruned" where message_date < ${
+      //       new Date(new Date().setMonth(new Date().getMonth() - 1)).getTime() *
+      //       1000
+      //     } and message != "This message has been pruned"`,
+      //     (err, results) => {
+      //       try {
+      //         if (err) throw err;
+      //         else {
+      //           console.log(`${results.changedRows} messages pruned`);
+      //         }
+      //       } catch (err) {
+      //         console.log("Error", err);
+      //       }
+      //     }
+      //   );
 
       sql.end();
     } catch (err) {
