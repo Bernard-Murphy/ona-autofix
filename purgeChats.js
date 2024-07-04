e3a4c9b5e4fef53ddce48c9a28be5a38d8f5b1d8
Bernard <bernard.murphy@rutgers.edu>
2024-07-04 04:13:19 -0400

    logs

--- purgeChats.js
+++ purgeChats.js
@@ -30,7 +30,29 @@ const purgeChats = () =>
               //     results.changedRows
               //   } messages pruned at ${new Date().toISOString()}`
               // );
-              console.log(results.length);
+              console.log("before", results.length);
+            }
+          } catch (err) {
+            console.log("Error", err);
+          }
+        }
+      );
+      sql.query(
+        // `update xf_conversation_message set user_id = "0", username = "guest", message = "This message has been pruned" where message_date < ${
+        //   new Date(new Date().setMonth(new Date().getMonth() - 1)).getTime() *
+        //   1000
+        // } and message != "This message has been pruned"`,
+        `select * from xf_conversation_message`,
+        (err, results) => {
+          try {
+            if (err) throw err;
+            else {
+              // console.log(
+              //   `${
+              //     results.changedRows
+              //   } messages pruned at ${new Date().toISOString()}`
+              // );
+              console.log("all", results.length);
             }
           } catch (err) {
             console.log("Error", err);
